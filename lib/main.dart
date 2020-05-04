3a0efd8eae5264bc897e6c6b0a4ac29f159c9c5b
purvatekade <purutekade15@gmail.com>
2020-05-04 17:12:01 +0530

    purva

---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'delayed_animation.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+  double _scale;
+  AnimationController _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.white;
+    _scale = 1 - _controller.value;
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+          backgroundColor: Colors.pinkAccent,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                AvatarGlow(
+                  endRadius: 80,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 0),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 10.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        backgroundImage: AssetImage('lib/images/img.jpg'),
+                        radius: 50, 
+                        
+                        ),
+                        // radius: 50.0,
+                      )),
+                SizedBox(
+                  height: 40,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Hi There",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    'I\'m Amigaa',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 2000,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Your Own",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Friend, Philosopher and Guide",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                SizedBox(
+                  height: 150.0,
+                ),
+                DelayedAnimation(
+                child: GestureDetector(
+                  onTapDown: _onTapDown,
+                  onTapUp: _onTapUp,
+                  child: Transform.scale(
+                    scale: _scale,
+                    child: _animatedButtonUI,
+                  ),
+                ),
+                delay: delayedAmount + 4000,
+              ),
+              SizedBox(height: 50.0,),
+                DelayedAnimation(
+                  child: Text(
+                    "I Already have An Account".toUpperCase(),
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 5000,
+                ),
+              ],
+            ),
+          )
+          //  Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: <Widget>[
+          //     Text('Tap on the Below Button',style: TextStyle(color: Colors.grey[400],fontSize: 20.0),),
+          //     SizedBox(
+          //       height: 20.0,
+          //     ),
+          //      Center(
+
+          //   ),
+          //   ],
+
+          // ),
+          ),
+    );
+  }
+
+  Widget get _animatedButtonUI => 
+  
+  Container(
+        height: 60,
+        width: 270,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Text(
+            'Hi Amiga',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.pinkAccent,
+            ),
+          ),
+        ),
+      );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+}
+
